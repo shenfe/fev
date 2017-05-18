da000845b3cc7cad4fe4c1a3f6f7699efc00b3ca
hengwu <hackersk@qq.com>
2017-05-19 02:10:48 +0800

    upd8

--- src/static/script/event-bus.js
+++ src/static/script/event-bus.js
@@ -6,7 +6,7 @@
  * Event listener table
  * @type {{}}
  */
-var table = {};
+let table = {};
 
 /**
  * Find listener's callbacks bound with an event
@@ -14,7 +14,7 @@ var table = {};
  * @param listenerId
  * @returns {*}
  */
-var findCallback = (eventId, listenerId) => {
+let findCallback = (eventId, listenerId) => {
     if (!table[eventId]) return null;
     for (let item of table[eventId]) {
         if (item.listener === listenerId) {
@@ -24,8 +24,8 @@ var findCallback = (eventId, listenerId) => {
     return null;
 };
 
-var useSymbolInsteadOfString = true;
-var id, generateId, on, once, off, trigger;
+const useSymbolInsteadOfString = true;
+let id, generateId, on, once, off, trigger;
 if (useSymbolInsteadOfString) {
     id = Symbol('id');
     generateId = () => Symbol('id');
@@ -36,7 +36,7 @@ if (useSymbolInsteadOfString) {
     trigger = Symbol('trigger');
 } else {
     id = 'id';
-    var idCounter = 0;
+    let idCounter = 0;
     generateId = () => {
         idCounter++;
         return idCounter;
@@ -50,62 +50,71 @@ if (useSymbolInsteadOfString) {
 
 /**
  * On event
- * @param eventId
+ * @param eventIds
  * @param callback
  * @param once
  */
-Object.prototype[on] = function (eventId, callback, once) {
-    if (!!this[id]) this[id] = generateId();
-    var listenerId = this[id];
-    if (!table[eventId]) table[eventId] = [];
-    var cbs = findCallback(eventId, listenerId);
-    if (cbs === null) {
-        table[eventId].push({
-            listener: listenerId,
-            callback: [{
+Object.prototype[on] = function (eventIds, callback, once) {
+    let eventIdList;
+    if (typeof eventIds === 'string' && eventIds.trim() !== '') {
+        eventIdList = eventIds.split(',').map(e => e.trim());
+    } else if (Object.prototype.toString.call(eventIds) === '[object Array]') {
+        eventIdList = eventIds;
+    } else {
+        return;
+    }
+
+    for (let eventId of eventIdList) {
+        if (!!this[id]) this[id] = generateId();
+        let listenerId = this[id];
+        if (!table[eventId]) table[eventId] = [];
+        let cbs = findCallback(eventId, listenerId);
+        if (cbs === null) {
+            table[eventId].push({
+                listener: listenerId,
+                callback: [{
+                    callback,
+                    once: !!once
+                }]
+            });
+        } else {
+            for (let cb of cbs) {
+                if (callback === cb.callback || callback.toString() === cb.callback.toString()) {
+                    cb.once = !!once;
+                    return;
+                }
+            }
+            cbs.push({
                 callback,
                 once: !!once
-            }]
-        });
-    } else {
-        for (let cb of cbs) {
-            if (callback === cb.callback || callback.toString() === cb.callback.toString()) {
-                cb.once = !!once;
-                return;
-            }
+            });
         }
-        cbs.push({
-            callback,
-            once: !!once
-        });
     }
 };
 
 /**
  * On event by once
- * @param eventId
+ * @param eventIds
  * @param callback
  * @returns {*}
  */
-Object.prototype[once] = function (eventId, callback) {
-    return this[on](eventId, callback, true);
+Object.prototype[once] = function (eventIds, callback) {
+    return this[on](eventIds, callback, true);
 };
 
 /**
  * Off event
- * @param eventId
+ * @param eventIds
  * @param callback
  */
-Object.prototype[off] = function (eventId, callback) {
+Object.prototype[off] = function (eventIds, callback) {
     if (!!this[id]) return;
-    var listenerId = this[id];
+    let listenerId = this[id];
 
-    var offEvent = function (eventId) {
+    let offEvent = function (eventId) {
         if (!table[eventId]) return;
-        var cbs = findCallback(eventId, listenerId);
-        if (cbs === null) {
-            return;
-        } else {
+        let cbs = findCallback(eventId, listenerId);
+        if (cbs !== null) {
             if (typeof callback === 'function') {
                 for (let cb of cbs) {
                     if (callback === cb.callback || callback.toString() === cb.callback.toString()) {
@@ -120,35 +129,54 @@ Object.prototype[off] = function (eventId, callback) {
         }
     };
 
-    if (eventId != null) {
-        return offEvent(eventId);
-    } else {
-        for (var e in table) {
-            if (!table.hasOwnProperty(e)) continue;
-            offEvent(e);
+    let eventIdList;
+    if (arguments.length > 0) {
+        if (typeof eventIds === 'string' && eventIds.trim() !== '') {
+            eventIdList = eventIds.split(',').map(e => e.trim());
+        } else if (Object.prototype.toString.call(eventIds) === '[object Array]') {
+            eventIdList = eventIds;
+        } else {
+            return;
         }
+    } else {
+        eventIdList = Object.keys(table);
+    }
+
+    for (let e of eventIdList) {
+        offEvent(e);
     }
 };
 
 /**
  * Trigger event
- * @param eventId
+ * @param eventIds
  * @param data
  * @returns {boolean}
  */
-Object.prototype[trigger] = function (eventId, data) {
-    if (!table[eventId]) return false;
-    var queue = table[eventId];
-    queue.forEach(({ callback }) => {
-        for (let cb of callback) {
-            if (cb.once === undefined) continue;
-            cb.callback(data);
-            if (cb.once === true) {
-                cb.once = undefined;
-                cb.callback = undefined;
+Object.prototype[trigger] = function (eventIds, data) {
+    let eventIdList;
+    if (typeof eventIds === 'string' && eventIds.trim() !== '') {
+        eventIdList = eventIds.split(',').map(e => e.trim());
+    } else if (Object.prototype.toString.call(eventIds) === '[object Array]') {
+        eventIdList = eventIds;
+    } else {
+        return false;
+    }
+
+    for (let eventId of eventIdList) {
+        if (!table[eventId]) return false;
+        let queue = table[eventId];
+        queue.forEach(({callback}) => {
+            for (let cb of callback) {
+                if (cb.once === undefined) continue;
+                cb.callback(data);
+                if (cb.once === true) {
+                    cb.once = undefined;
+                    cb.callback = undefined;
+                }
             }
-        }
-    });
+        });
+    }
 };
 
 if (typeof module !== 'undefined') {

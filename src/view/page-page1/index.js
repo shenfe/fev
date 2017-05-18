82630f5f6146034f4cc28d038b567f75a81a6cd9
keshen <hackersk@qq.com>
2017-05-18 11:51:14 +0800

    upd8

---
+++ src/view/page-page1/index.js
@@ -0,0 +1,12 @@
+import util from '../../static/script/util';
+var helper = require('../../static/script/util');
+
+import module1 from '../module-module1';
+
+var other = require('./other');
+
+import b from './other2';
+
+export default page1
+
+module.exports = page1;

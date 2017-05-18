4fa76cc24a5c6fac295a9aa9fdd54279004ed5bb
keshen <hackersk@qq.com>
2017-05-18 20:33:49 +0800

    upd8: event.

--- src/controller/event-bus.js
+++ src/controller/event-bus.js
@@ -2,4 +2,37 @@
  * Created by godzilla on 5/18/17.
  */
 
-var viewEventQueue = {};
+var table = {};
+
+var callbackQueueHas = (eventId, listenerId) => {
+    if (!table[eventId]) return false;
+    for (let item of table[eventId]) {
+        if (item.listener === listenerId) {
+            return item;
+        }
+    }
+    return false;
+};
+
+module.exports = {
+    on: (eventId, callback) => {
+        var listenerId = this.guid || 'unknown';
+        if (!table[eventId]) table[eventId] = [];
+        var check = callbackQueueHas(eventId, listenerId);
+        if (check === false) {
+            table[eventId].push({
+                listener: listenerId,
+                callback: [callback]
+            });
+        } else {
+            var callbackList = check.callback;
+            for (let cb of callbackList) {
+                if (callback === cb) return;
+            }
+            callbackList.push(callback);
+        }
+    },
+    once: (id, callback) => {},
+    off: (id, callback) => {},
+    trigger: data => {}
+};
